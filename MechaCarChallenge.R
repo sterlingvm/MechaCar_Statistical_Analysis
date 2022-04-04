671acf7463cf8d2c41af23598cfd1e95bfaa052c
sterlingvm <sterlingvmiller1@gmail.com>
2022-04-04 02:37:50 -0500

    Revised variables in statistical tests and re-generated source image

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -29,10 +29,10 @@ summary(lm(mpg ~ vehicle_length + ground_clearance, data=mecha_mpg))
 ## Deliverable 2
 
 #2. Import and read in the Suspension_Coil.csv file as a table
-mecha_coil <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+coil_mechacar <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
 
 #3. Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
-total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+total_summary <- coil_mechacar %>% summarize(Mean_PSI=mean(PSI),
                                           Median_PSI=median(PSI),
                                           Var_PSI=var(PSI),
                                           Std_Dev_PSI=sd(PSI),
@@ -42,7 +42,7 @@ total_summary
 
 #4. Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to 
 # group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
-lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+lot_summary <- coil_mechacar  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
                                                                          Median_PSI=median(PSI),
                                                                          Var_PSI=var(PSI),
                                                                          Std_Dev_PSI=sd(PSI),
@@ -50,12 +50,12 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
 # Check dataframe
 lot_summary
 
-# Visualize the mecha_coil data - box plot (WHOLE LOT):
-plt1 <- ggplot(mecha_coil,aes(y=PSI)) #import dataset into ggplot2
+# Visualize the coil_mechacar data - box plot (WHOLE LOT):
+plt1 <- ggplot(coil_mechacar,aes(y=PSI)) #import dataset into ggplot2
 plt1 + geom_boxplot() #add boxplot
 
-# Visualize the mecha_coil data - box plot (BY LOT):
-plt2 <- ggplot(mecha_coil,aes(x=Manufacturing_Lot,y=PSI)) #import dataset into ggplot2
+# Visualize the coil_mechacar data - box plot (BY LOT):
+plt2 <- ggplot(coil_mechacar,aes(x=Manufacturing_Lot,y=PSI)) #import dataset into ggplot2
 plt2 + geom_boxplot()
 
 
@@ -63,14 +63,14 @@ plt2 + geom_boxplot()
 ## Deliverable 3
 
 #1. use t.test() to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
-t.test(mecha_coil$PSI,mu=1500)
+t.test(coil_mechacar$PSI,mu=1500)
 # p-value > 0.05
 
 
 #2. Write 3 t.test() functions using subset() to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
-lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
-lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
-lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+lot1 <- subset(coil_mechacar, Manufacturing_Lot=="Lot1")
+lot2 <- subset(coil_mechacar, Manufacturing_Lot=="Lot2")
+lot3 <- subset(coil_mechacar, Manufacturing_Lot=="Lot3")
 
 t.test(lot1$PSI,mu=1500)
 t.test(lot2$PSI,mu=1500)
